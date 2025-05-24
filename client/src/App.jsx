4f9d02bda6be7338628c96b432f30c1588a3bc26
AryanRajTalekar <aryanrajtalekar@gmail.com>
2025-05-24 12:44:31 +0530

    client and server folders created

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-xl font-semibold w-full h-screen bg-gray-800'>
+      {/* <img src="\public\Frame 2.png" alt="" /> */}
+      <img src="\public\Think Stack.png" alt="" />
+    </div>
+  )
+}
+
+export default App 
